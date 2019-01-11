f2e24d3446105f2fe257480c140d0204e587ea33
Greyson Richey <greysonrichey@gmail.com>
2019-01-10 21:47:34 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-A react-ified version of [Quether: The Quick Weather App](https://quether.herokuapp.com/).
-
-The original source can be found [here](https://github.com/Bloodyaugust/weather-forecast). This project does not replicate the API behavior.
-
-This project was done in ~4 hours, and has not been optimized for React. Thus, it's a bit ugly in some places...
---
+++ precache-manifest.56e9b2629834dbb558d61db562eaad23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf20c2a5216ab184f86",
+    "url": "/quether-react/static/js/runtime~main.1cf20c2a.js"
+  },
+  {
+    "revision": "875bf4e85238ed396378",
+    "url": "/quether-react/static/js/main.875bf4e8.chunk.js"
+  },
+  {
+    "revision": "9a89dccbf27644a5879a",
+    "url": "/quether-react/static/js/1.9a89dccb.chunk.js"
+  },
+  {
+    "revision": "875bf4e85238ed396378",
+    "url": "/quether-react/static/css/main.d1deea8c.chunk.css"
+  },
+  {
+    "revision": "8f36973278641f4d812933ab65185c11",
+    "url": "/quether-react/index.html"
+  }
+];
